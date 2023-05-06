c17b08f12c466a8c24eaa395146ce94ef0d7ef6a
famouscat <onismaa@gmail.com>
2023-05-06 22:42:49 +0800

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+# A vite template project
+
+using React, Typescript, Jotai, Mui.com
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/joy";
+
+function App() {
+  return (
+    <>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
